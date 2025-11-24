504f9951091d6178ef3ba8d85e3ebef3933864c9
xxCosta <langley.ben98@gmail.com>
2025-11-23 20:50:11 -0400

    fast-forward to next session 99% working
    the e key now fast forwards to the same time of the next day no matter what time you're at. doesnt work when the market closes for the weekend so thats the next bug i need to fix but other than that it works

--- replay_manager.cpp
+++ replay_manager.cpp
@@ -10,13 +10,13 @@ SCSFExport scsf_ToggleJump(SCStudyInterfaceRef sc)
     int chartNum = 2;
     static int speed = 4;
     static bool fastForwardActive = false;
-    static int replaySpeedPerSecond = 0;
+    static int replaySpeedPerSecond;
     static int fastSpeed = 0;
 
     if (sc.SetDefaults)
     {
         sc.GraphName = "Replay Manager";
-        sc.AutoLoop = 0;
+        sc.AutoLoop = 1;
         sc.UpdateAlways = 1;
         sc.ReceiveKeyboardKeyEvents = 1; 
         sc.SupportKeyboardModifierStates = 1;
@@ -56,11 +56,12 @@ SCSFExport scsf_ToggleJump(SCStudyInterfaceRef sc)
     // === E key: fast forward to next session in ~2 seconds ===
      
     static SCDateTime repeatUntil2SecondsLater;
-    
+    static int futureBarIndex;
+    static int lastVisibleBarIndex;
     if (keyboardCode == 69) //&& !fastForwardActive)
     {
         SCDateTime latestBarTime = sc.LatestDateTimeForLastBar;
-        sc.AddMessageToLog(sc.DateTimeToString(latestBarTime, FLAG_DT_HOUR), 0);
+        // sc.AddMessageToLog(sc.DateTimeToString(latestBarTime, FLAG_DT_HOUR), 0);
         int currentTimeInHour = latestBarTime.GetHour();
         int goalTime = 20; 
         int hoursTilNextSesh;
@@ -69,29 +70,29 @@ SCSFExport scsf_ToggleJump(SCStudyInterfaceRef sc)
         } else {
             hoursTilNextSesh = goalTime - currentTimeInHour;    
         }
-        sc.AddMessageToLog(SCString().Format("%d hrs till next session",hoursTilNextSesh),0);
+        //sc.AddMessageToLog(SCString().Format("%d hrs till next session",hoursTilNextSesh),0);
         int secondsTilNextSesh = hoursTilNextSesh * 3600;
         replaySpeedPerSecond = secondsTilNextSesh/2;
         // dont delete the log, it's a good example of a one line log for numbers
-        sc.AddMessageToLog(SCString().Format("%d",replaySpeedPerSecond),0);
+        //sc.AddMessageToLog(SCString().Format("%d",replaySpeedPerSecond),0);
+        
+        lastVisibleBarIndex = sc.IndexOfLastVisibleBar;
+        sc.AddMessageToLog(SCString().Format("%d",lastVisibleBarIndex),0);
 
         repeatUntil2SecondsLater = sc.CurrentSystemDateTime.AddSeconds(2);
+        futureBarIndex = lastVisibleBarIndex + hoursTilNextSesh;
+        sc.AddMessageToLog(SCString().Format("%d", futureBarIndex),0);
+        fastForwardActive = true;
+        sc.ChangeChartReplaySpeed(chartNum, replaySpeedPerSecond); 
 
     }
 
-    if(sc.CurrentSystemDateTime < repeatUntil2SecondsLater){
-        if(fastForwardActive == false){
-            fastForwardActive = true;
-            sc.ChangeChartReplaySpeed(chartNum, replaySpeedPerSecond); 
-            // sc.AddMessageToLog("started fast forward",0);
-        };
-    }
 
-    if(sc.CurrentSystemDateTime == repeatUntil2SecondsLater){
-        if(fastForwardActive == true) {
-            fastForwardActive = false;
-            sc.ChangeChartReplaySpeed(chartNum, 4); 
-            // sc.AddMessageToLog("done fast forawrd",0);
+    if(fastForwardActive) {
+        if(sc.IndexOfLastVisibleBar >= futureBarIndex){
+        sc.ChangeChartReplaySpeed(chartNum, 4); 
+        // sc.AddMessageToLog("done fast forawrd",0);
+        fastForwardActive = false;
         }
     }
 
