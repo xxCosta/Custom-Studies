853e4b330402ec836cf9633286a9c8e6c605a2fb
xxCosta <langley.ben98@gmail.com>
2025-12-06 23:20:11 -0400

    skeleton mapped out
    the deeper i get into this the more i realize this is wont be a quick project to code. there's a lot i didnt account for and i think ill have to refactor it. Since I have a better understanding of orders, i'm going to put this on the backburner temporarily and enter positions manually until i can figure this app out.

--- entryboi.cpp
+++ entryboi.cpp
@@ -15,12 +15,18 @@ SCSFExport scsf_rectangleBoxEntry(SCStudyInterfaceRef sc)
     SCInputRef rr = sc.Input[0];
     rr.Name = "risk:reward ratio";
     rr.SetFloat(1);
+    
+    // SETTING TRADE MODE
+    SCInputRef isTradeModeActive = sc.Input[1];
+    isTradeModeActive.Name = "Trade Mode Active?";
+    isTradeModeActive.SetYesNo(1);
+    bool tradeMode = isTradeModeActive.GetBoolean();
 
 	// Section 1 - Set the configuration variables and defaults
 	if (sc.SetDefaults)
 	{
 		sc.GraphName = "Entry Boi - S&D Entry";
-        sc.UpdateAlways = 0;
+        sc.UpdateAlways = 1;
 		sc.AutoLoop = 1; 
 		sc.HideStudy = 1;	
 		return;
@@ -28,26 +34,63 @@ SCSFExport scsf_rectangleBoxEntry(SCStudyInterfaceRef sc)
 
     int chartNum = 2;	
     s_UseTool entryBox;	
-    float& slPriceBottomOfBox = sc.GetPersistentFloat(0);
-    float& entryPriceTopOfBox = sc.GetPersistentFloat(1);
+    float& slPrice = sc.GetPersistentFloat(0);
+    float& entryPrice = sc.GetPersistentFloat(1);
     float& tpPrice = sc.GetPersistentFloat(2);
     
     
 
     if(sc.GetUserDrawnChartDrawing(chartNum, DRAWING_RECTANGLEHIGHLIGHT, entryBox, -1)){
 
-        if(slPriceBottomOfBox != entryBox.BeginValue || entryPriceTopOfBox != entryBox.EndValue ){
-            slPriceBottomOfBox = entryBox.BeginValue;
-            entryPriceTopOfBox = entryBox.EndValue;
+        if(slPrice != entryBox.BeginValue || entryPrice != entryBox.EndValue ){
+            slPrice = entryBox.BeginValue;
+            entryPrice = entryBox.EndValue;
 
-            float tp = ((entryPriceTopOfBox - slPriceBottomOfBox)*rr.GetFloat()) + entryPriceTopOfBox;
+            float tp = ((entryPrice - slPrice)*rr.GetFloat()) + entryPrice;
+            tpPrice = tp; 
 
-            sc.AddMessageToLog(SCString().Format("entry at %.3f", entryPriceTopOfBox),0);
+            sc.AddMessageToLog(SCString().Format("entry at %.3f", entryPrice),0);
             sc.AddMessageToLog(SCString().Format("tp at%.3f", tp),0);
-            sc.AddMessageToLog(SCString().Format("sl  at %.3f", slPriceBottomOfBox),0);
+            sc.AddMessageToLog(SCString().Format("sl  at %.3f", slPrice),0);
+        }
+
+        // ORDER STUFF
+        int& entryOrderID = sc.GetPersistentInt(0);
+        s_SCNewOrder entryOrder;
+        entryOrder.OrderQuantity = 1;
+        entryOrder.OrderType = SCT_ORDERTYPE_LIMIT;
+        entryOrder.Price1 = entryPrice;
+        entryOrder.TimeInForce = SCT_TIF_GOOD_TILL_CANCELED;
+        s_SCNewOrder stopOrder;
+        s_SCNewOrder tpOrder;
+
+        // RUN DAT CRODIE
+        if(tradeMode){
+            if(tpPrice>slPrice){
+                if(entryOrderID <= 0){
+                    int orderPlaced = sc.BuyEntry(entryOrder);
+                    entryOrderID = orderPlaced;
+                }
+            }else {
+
+                if(entryOrderID <= 0){
+                    int orderPlaced = sc.SellEntry(entryOrder);
+                    entryOrderID = orderPlaced;
+                }
+
+
+
+
+            }
         }
 
     }
+
+
+
+    
+    
+    
 
 
     
