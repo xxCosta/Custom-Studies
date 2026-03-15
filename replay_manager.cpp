43d694055f1c65ca698fd642bd6341e6ff71acaa
xxCosta <langley.ben98@gmail.com>
2026-03-14 22:43:24 -0300

    Rewrote the study
    i was trying to use this tool on a volume based chart and it didnt work
    so i decided to rewrite it in a better way. I know a lot more about c++,
    acsil and sierra chart than i did when i first wrote this and i felt
    instead of fixing broken code i might as well fix it the right way.
    this works a lot better and is simpler. it is a lot moire accurate, it
    never skips past the time you set. very happy with this instrument
    agnosticity.

--- replay_manager.cpp
+++ replay_manager.cpp
@@ -1,124 +1,45 @@
 #include "sierrachart.h"
+#include <algorithm>
 #include <vector>
-
-
-
 
 SCDLLName("replay-manager")
 
-SCSFExport scsf_ToggleJump(SCStudyInterfaceRef sc)
-{
-  static std::vector<int> chartNums = {2,3,1,6,8}; 
-  // int chartNum2 = sc.ChartNumber;
-  int chartNum2 = 2;
-  int chartNum8 = 8;
-  static int speed = 4;
-  static bool fastForwardActive = false;
-  static int replaySpeedPerSecond;
-  static int fastSpeed = 0;
-
-  if (sc.SetDefaults)
-  {
+    SCSFExport scsf_ToggleJump(SCStudyInterfaceRef sc) {
+  if (sc.IsFullRecalculation || sc.DownloadingHistoricalData)
+    return;
+  if (sc.SetDefaults) {
     sc.GraphName = "Replay Manager";
     sc.AutoLoop = 1;
     sc.UpdateAlways = 1;
-    sc.ReceiveKeyboardKeyEvents = 1; 
-    sc.SupportKeyboardModifierStates = 1;
+
+    sc.MaintainAdditionalChartDataArrays = 1;
+
     return;
   }
 
-  int keyboardCode = sc.KeyboardKeyEventCode;
+  int &ffActive = sc.GetPersistentInt(1);
+  int &ffInSession = sc.GetPersistentInt(2);
 
-  // D key logic: init/start/pause/resume replay
-  if (keyboardCode == 68)
-  {
-    for(int chartNum : chartNums){
-      int replayStatus = sc.GetReplayStatusFromChart(chartNum);
+  SCSubgraphRef currentTimeS = sc.Subgraph[0];
+  currentTimeS.Name = "Current Time in Seconds";
+  int goalTime = 68200; // in seconds
 
-      if (replayStatus == 0) 
-      {
-        SCDateTime replayDate;
-        // reset when starting a new backtest
-        // replayDate.SetDateTimeYMDHMS(2024, 01, 15, 0, 0, 0);
-
-        replayDate.SetDateTimeYMDHMS(2024, 02, 11, 0, 0, 0);
-
-        n_ACSIL::s_ChartReplayParameters replayParams;
-        replayParams.ChartNumber = chartNum;
-        replayParams.ReplaySpeed = 1;
-        replayParams.StartDateTime = replayDate;
-
-        sc.StartChartReplayNew(replayParams);
-        sc.ResumeChartReplay(chartNum);
-      }
-      else if (replayStatus == 2)
-      {
-        sc.ResumeChartReplay(chartNum);
-      }
-      else if (replayStatus == 1)
-      {
-        sc.PauseChartReplay(chartNum);
-      }
-    }
+  currentTimeS[sc.Index] = sc.BaseDateTimeIn[sc.Index].GetTimeInSeconds();
+  int ct = currentTimeS[sc.Index];
+  if (sc.GetCustomStudyControlBarButtonEnableState(3)) {
+    sc.ChangeChartReplaySpeed(sc.ChartNumber, 17000);
+    ffActive = true;
   }
 
-  // === E key: fast forward to next session in ~2 seconds ===
+  sc.SetCustomStudyControlBarButtonEnable(3, 0);
 
-
-
-
-  static SCDateTime repeatUntil2SecondsLater;
-  static int futureBarIndex;
-  static int lastVisibleBarIndex;
-
-
-  if (keyboardCode == 69) //&& !fastForwardActive)
-  {
-    SCDateTime latestBarTime = sc.LatestDateTimeForLastBar;
-    // sc.AddMessageToLog(sc.DateTimeToString(latestBarTime, FLAG_DT_HOUR), 0);
-    int currentTimeInHour = latestBarTime.GetHour();
-    int goalTime = 20; 
-    int hoursTilNextSesh;
-    if(currentTimeInHour >= goalTime){
-      hoursTilNextSesh = (goalTime - currentTimeInHour) + 24;
-    } else {
-      hoursTilNextSesh = goalTime - currentTimeInHour;    
-    }
-    //sc.AddMessageToLog(SCString().Format("%d hrs till next session",hoursTilNextSesh),0);
-    int secondsTilNextSesh = hoursTilNextSesh * 3600;
-    replaySpeedPerSecond = secondsTilNextSesh/2;
-    // dont delete the log, it's a good example of a one line log for numbers
-    //sc.AddMessageToLog(SCString().Format("%d",replaySpeedPerSecond),0);
-
-    lastVisibleBarIndex = sc.IndexOfLastVisibleBar;
-    sc.AddMessageToLog(SCString().Format("%d",lastVisibleBarIndex),0);
-
-    futureBarIndex = lastVisibleBarIndex + hoursTilNextSesh;
-    sc.AddMessageToLog(SCString().Format("%d", futureBarIndex),0);
-    fastForwardActive = true;
-    for(int chartNum : chartNums){
-      sc.ChangeChartReplaySpeed(chartNum, replaySpeedPerSecond); 
-    }
+  if (ffActive && ct >= goalTime && !ffInSession) {
+    sc.ChangeChartReplaySpeed(sc.ChartNumber, 1);
+    ffActive = false;
+    ffInSession = true;
   }
 
-
-  if(fastForwardActive) {
-    if(sc.IndexOfLastVisibleBar >= futureBarIndex){
-      for(int chartNum : chartNums){
-        sc.ChangeChartReplaySpeed(chartNum, 1); 
-      }
-      // sc.AddMessageToLog("done fast forawrd",0);
-      fastForwardActive = false;
-    }
-  }
-
-  // F key logic: toggle replay speed
-  if (keyboardCode == 70)
-  {
-    speed = (speed == 4000) ? 1 : 4000;
-    for(int chartNum : chartNums){
-
-      sc.ChangeChartReplaySpeed(chartNum, speed);
-    }
+  if (ct < goalTime) {
+    ffInSession = false;
   }
 }
