aa7948394e40d1a3460d90b0159668e6c279a967
xxCosta <langley.ben98@gmail.com>
2026-03-18 17:41:59 -0300

    multiple charts in sync
    i had to do a full rewrite but it was worth it. I discovered that each
    chart has its own replay, which made syncing a breeze. now each chart
    creates its own stop points meaning it will never skip. only starting
    the charts is synchronized, after that they work independently. I'm
    really proud of my rewrite. Although I know there is a better pattern,
    I don't need to discover and implement it now. I fixed the issue that
    needs to be fixed and added some ease of use inputs; i've improved the
    program. Slowly it will get better and better. Great job me!

--- replay_manager.cpp
+++ replay_manager.cpp
@@ -1,25 +1,44 @@
 #include "sierrachart.h"
-#include <algorithm>
-#include <vector>
-
 SCDLLName("replay-manager")
 
-    SCSFExport scsf_ToggleJump(SCStudyInterfaceRef sc) {
-  if (sc.IsFullRecalculation || sc.DownloadingHistoricalData)
-    return;
+int replaySpeed = 16000;
+
+SCSFExport scsf_ReplayMaster(SCStudyInterfaceRef sc) {
   if (sc.SetDefaults) {
-    sc.GraphName = "Replay Manager";
+    sc.GraphName = "Replay Manager - Master";
+    sc.GraphRegion = 1;
     sc.AutoLoop = 1;
     sc.UpdateAlways = 1;
 
+    sc.Input[0].Name = "start date";
+    sc.Input[0].SetDate(0);
+
+    sc.Input[1].Name = "Goal Time (0-23)";
+    sc.Input[1].SetInt(18);
     sc.MaintainAdditionalChartDataArrays = 1;
 
     return;
   }
 
+  if (sc.IsFullRecalculation || sc.DownloadingHistoricalData)
+    return;
+
+  SCDateTime replayStartDate = sc.Input[0].GetDate();
+  sc.SetPersistentSCDateTime(1, replayStartDate);
+  n_ACSIL::s_ChartReplayParameters rparams;
+  rparams.ReplaySpeed = 1;
+  rparams.StartDateTime = replayStartDate;
+  rparams.SkipEmptyPeriods = 0;
+  rparams.ReplayMode = n_ACSIL::REPLAY_MODE_STANDARD;
+  rparams.ClearExistingTradeSimulationDataForSymbolAndTradeAccount = 0;
+
+  if (sc.GetCustomStudyControlBarButtonEnableState(4)) {
+    sc.StartChartReplayNew(rparams);
+    sc.SetCustomStudyControlBarButtonEnable(4, 0);
+  }
+
   int &ffActive = sc.GetPersistentInt(1);
   int &ffInSession = sc.GetPersistentInt(2);
-
   SCSubgraphRef currentTimeS = sc.Subgraph[0];
   currentTimeS.Name = "Current Time in Seconds";
   int goalTime = 68200; // in seconds
@@ -27,7 +46,7 @@ SCDLLName("replay-manager")
   currentTimeS[sc.Index] = sc.BaseDateTimeIn[sc.Index].GetTimeInSeconds();
   int ct = currentTimeS[sc.Index];
   if (sc.GetCustomStudyControlBarButtonEnableState(3)) {
-    sc.ChangeChartReplaySpeed(sc.ChartNumber, 17000);
+    sc.ChangeChartReplaySpeed(sc.ChartNumber, replaySpeed);
     ffActive = true;
   }
 
@@ -41,5 +60,82 @@ SCDLLName("replay-manager")
 
   if (ct < goalTime) {
     ffInSession = false;
+  }
+}
+
+SCSFExport scsf_ReplaySlave(SCStudyInterfaceRef sc) {
+
+  if (sc.SetDefaults) {
+    sc.GraphName = "Replay Manager - Slave";
+    sc.GraphRegion = 1;
+    sc.AutoLoop = 1;
+    sc.UpdateAlways = 1;
+
+    sc.Input[0].Name = "start date";
+    sc.Input[0].SetDate(0);
+
+    sc.Input[1].Name = "Master Chart Number";
+    sc.Input[1].SetInt(3);
+
+    sc.Input[2].Name = "Master Chart Study ID";
+    sc.Input[2].SetInt(8);
+
+    sc.MaintainAdditionalChartDataArrays = 1;
+
+    return;
+  }
+  if (sc.IsFullRecalculation || sc.DownloadingHistoricalData)
+    return;
+
+  struct mParams {
+    int ChartNumber;
+    int StudyId;
+    SCDateTime Date;
+    bool ffActive;
+  } m;
+
+  m.ChartNumber = sc.Input[1].GetInt();
+  m.StudyId = sc.Input[2].GetInt();
+  m.Date = sc.GetPersistentSCDateTimeFromChartStudy(m.ChartNumber, m.StudyId, 1);
+
+  if (sc.GetCustomStudyControlBarButtonEnableState(4)) {
+    n_ACSIL::s_ChartReplayParameters rparams;
+    rparams.ReplaySpeed = 1;
+    rparams.StartDateTime = m.Date;
+    rparams.SkipEmptyPeriods = 0;
+    rparams.ReplayMode = n_ACSIL::REPLAY_MODE_STANDARD;
+    rparams.ClearExistingTradeSimulationDataForSymbolAndTradeAccount = 0;
+    sc.StartChartReplayNew(rparams);
+    sc.SetCustomStudyControlBarButtonEnable(4, 0);
+  }
+
+  int &ffActiveSlave = sc.GetPersistentInt(5);
+  int &ffInSessionSlave = sc.GetPersistentInt(6);
+  SCSubgraphRef currentTimeS = sc.Subgraph[0];
+  currentTimeS.Name = "Current Time in Seconds";
+  int goalTime = 68200; // in seconds
+
+  m.ffActive = sc.GetPersistentIntFromChartStudy(m.ChartNumber, m.StudyId, 1);
+
+  currentTimeS[sc.Index] = sc.BaseDateTimeIn[sc.Index].GetTimeInSeconds();
+  int ct = currentTimeS[sc.Index];
+  float currentReplaySpeed = sc.GetChartReplaySpeed(sc.ChartNumber);
+  if (currentReplaySpeed > 1000) {
+    sc.AddMessageToLog("turned off master tracking", 0);
+    m.ffActive = false;
+  }
+  if (m.ffActive) {
+    sc.ChangeChartReplaySpeed(sc.ChartNumber, replaySpeed);
+    ffActiveSlave = true;
+  }
+
+  if (ffActiveSlave && ct >= goalTime && !ffInSessionSlave) {
+    sc.ChangeChartReplaySpeed(sc.ChartNumber, 1);
+    ffActiveSlave = false;
+    ffInSessionSlave = true;
+  }
+
+  if (ct < goalTime) {
+    ffInSessionSlave = false;
   }
 }
